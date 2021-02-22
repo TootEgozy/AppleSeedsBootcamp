fc79b13ad6d200cd92d5870515248745834eac41
Toot Egozy <strawberevil@gmail.com>
2021-02-22 19:35:29 +0200

    new exercise

---
+++ Exercises/JS/17.2_text_area/17.2.js
@@ -0,0 +1,17 @@
+
+let textBox = document.querySelector(".text-box");
+
+
+let button = document.querySelector(".submit");
+
+let errorText = document.querySelector(".error");
+
+button.addEventListener('click', setError);
+
+function setError () {
+    if (textBox.value.length < 100) {
+        errorText.innerHTML = "Please enter at least 100 chars";
+        textBox.style.border = "2px solid red";
+    }
+}
+
