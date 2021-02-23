2fb5930239cfe6adc7b3af8df004903ddc617390
Toot Egozy <strawberevil@gmail.com>
2021-02-23 13:51:37 +0200

    new exercise

--- Exercises/JS/17.2_text_area/17.2.js
+++ Exercises/JS/17.2_text_area/17.2.js
@@ -1,17 +1,19 @@
 
 let textBox = document.querySelector(".text-box");
 
-
 let button = document.querySelector(".submit");
 
 let errorText = document.querySelector(".error");
 
-button.addEventListener('click', setError);
+button.addEventListener('click', submit);
 
-function setError () {
+function submit() {
+
     if (textBox.value.length < 100) {
         errorText.innerHTML = "Please enter at least 100 chars";
         textBox.style.border = "2px solid red";
     }
 }
+
+
 
---
+++ Exercises/JS/18.1_smiley/18.1.js
@@ -0,0 +1,51 @@
+//function that recives a number that is inputed by the user,
+//and sets new html elements "img" for that number.
+
+let imgSrc = 'smiley.png';
+let inputFiled = document.querySelector('.input-filed');
+let submitBtn = document.querySelector('.submit');
+let myP = document.createElement('p');
+myP.textContent = "Please enter a positive number";
+
+
+
+function handleInput () {
+    let input = document.querySelector('.input-filed').value;
+    if (typeof Number(input) == "number" && input > 0) {
+
+        console.log(input);
+        console.log("creating img");
+        for(i = 0; i < input; i++) {
+            let myImg = document.createElement('img');
+            myImg.src = imgSrc;
+            myImg.style.width = '150px';
+            document.body.insertAdjacentElement('afterend', myImg);
+        }
+    }
+    else {
+        console.log(input);
+        console.log("else condition");
+           
+        //    let myPFragment = document.createRange().createContextualFragment(myP);
+        //     myPFragment.classList.add('.error'); this doesnwt work. why?
+        //     inputFiled.classList.add('.error');
+        document.body.insertAdjacentElement('afterend', myP);
+        inputFiled.style.border = "2px solid red";
+    }
+}
+
+function tests () {
+    inputFiled.style.border = "2px solid red";
+    //inputFiled.classList.add('.error'); this doesnt work. why?
+
+    //let myPFragment = document.createRange().createContextualFragment(myP);
+
+    //document.body.insertAdjacentElement('afterend', myPFragment);
+    //myPFragment.classList.add('.error');
+    console.log(p);
+
+   
+
+}
+
+submitBtn.addEventListener('click', handleInput);
