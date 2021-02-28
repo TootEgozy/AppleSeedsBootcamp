3b1c9977a70868271f1e046b8663ff686ac0c2ba
Toot Egozy <strawberevil@gmail.com>
2021-02-28 13:31:27 +0200

    new exercises

---
+++ Exercises/JS/27.1_is_square/27.1_is_square.js
@@ -0,0 +1,25 @@
+//Using prototype properties, add a method to Square named isSquare that
+//prints true if a Square object has equal edges and false if they are unequal.
+//Here is the Square function:
+
+function Square(a, b, c, d){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+}
+
+Square.prototype.isSquare = function () {
+    if (this.a == this.b == this.c == this.b) return true;
+    else return false;
+}
+
+//remember, you call the object *prototype* methods only
+//on the objects that were created from the tamplate,
+//not on the template itself
+
+let mySquare = new Square(1,1,1,1);
+let myRectangle = new Square(1,2,1,2);
+
+console.log(mySquare.isSquare());
+console.log(myRectangle.isSquare());
