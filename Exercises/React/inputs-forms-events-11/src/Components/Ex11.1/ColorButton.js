64d9a9abddebca4fb884d4d16bbb7dae0a54a206
Toot Egozy <strawberevil@gmail.com>
2021-03-18 19:39:48 +0200

    new exercise

--- Exercises/React/inputs-forms-events-11/src/Components/Ex11.1/ColorButton.js
+++ Exercises/React/inputs-forms-events-11/src/Components/Ex11.1/ColorButton.js
@@ -4,21 +4,23 @@ import './ColorButtons.css'
 class ColorButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {buttoncolor: this.props.color}
+        this.state = {color: this.props.color}
+        console.log("New Button is made! Color "+this.props.color);
+        console.log(props);
     }
     componentDidMount () {
-        console.log(this.state.buttoncolor);
+        console.log(this.state.color);
     }
     changeMyColor=()=> {
-        this.props.changeColorState(this.state.buttoncolor);
+        this.props.changeColorState(this.state.color);
     }
     render () {
         return (
             <button 
-            style={{backgroundColor:this.state.buttoncolor}} 
+            style={{backgroundColor:this.state.color}} 
             onClick={this.changeMyColor}
             >
-                {this.state.buttoncolor}
+                {this.state.color}
             </button>
         )
     }
--- Exercises/React/inputs-forms-events-11/src/Components/Ex11.1/ColorParent.js
+++ Exercises/React/inputs-forms-events-11/src/Components/Ex11.1/ColorParent.js
@@ -6,23 +6,21 @@ import './ColorButtons.css'
 const colors = ["red", "blue", "yellow"];
 
 class ColorParent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {color: ""}
-  }
+    constructor(props) {
+        super(props);
+        this.state = {color: ""};    
+    }
 
-  changeMyColor =(newColor) => {
-    this.setState({color: newColor});
-    console.log(this.state.color)
-
-  }
+    changeMyColor =(newColor) => {
+        this.setState({color: newColor});
+    }
   
-  buttonRender () {
-      colors.map(color => <ColorButton color={color}/>)
-  }
+    buttonRender () {
+        colors.map(color => <ColorButton color={color}/>);
+    }
 
-  render () { 
-    return <fieldset className="a4">
+    render () { 
+        return <fieldset className="a4">
                 {colors.map(color => <ColorButton color={color} changeColorState={this.changeMyColor}/>)}
                 <div className="a3" style={{color:this.state.color}}>{this.state.color}</div>
            </fieldset>
